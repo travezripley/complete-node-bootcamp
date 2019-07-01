0c5f5f3658cbd2534b4c18212d8cee5bdf596a20
Travis Ripley <Theripleys@gmail.com>
2019-06-30 22:44:46 -0700

    adds routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+/////////////////////////////////////////////////////////////
+// Files
 
 //blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -9,18 +14,43 @@ const fs = require('fs');
 // console.log('File has been written');
 
 //Non-locking, synchronous way
-fs.readFile('./txt/start.txt','utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!,')
-  fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt','utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/start.txt','utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!,')
+//   fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt','utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Files have been written 🥑');
-      })
+//       fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Files have been written 🥑');
+//       })
+//     });
+//   });
+// });
+// console.log("Will read file !");
+
+/////////////////////////////////////////////////////////////
+// Server
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  
+  if(pathName === '/' || pathName === '/overview') {
+    res.end('This is the overview!');
+  } else if  (pathName === '/product') {
+    res.end('This is the product!');
+  }else{
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello world'
+
     });
-  });
-});
-console.log("Will read file !");
+    res.end('<h1>Page not found!</h1>');
+  }
 
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listing to requests on port 8000')
+
+});
